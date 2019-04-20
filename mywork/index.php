d52105430fc5c8a9fdb73b97a44dea08084485c9
Avizit Chowdhury <49820912+avizitchy@users.noreply.github.com>
2019-04-21 02:36:15 +0600

    Add files via upload

---
+++ mywork/index.php
@@ -0,0 +1,76 @@
+<?php 
+/*Print String*/
+	// echo 123456789;
+	// echo "Lorem ipsum dolar";
+	// echo "<br>";
+	// echo "Lorem ipsum dolar";
+/*Variable data store*/
+	// $test = "Hello Bangladesh";
+	// $number = 100;
+	// $numberTwo = 50;
+	// $data = $number + $numberTwo;
+	// $number = 500;
+/*Define Constant*/
+// define('Age', 20);
+// define('test', 'Hello Bangladesh');
+// echo Age;
+// define('Age1', 70);
+/* Concate Data */
+	// $test1 = 'Hello Bangladesh';
+	// $test2 = 'Welcome To Bangladesh';
+	// // $data = $test1.' '.$test2;
+	// $data = "Hi my name Bappa Please".' '.$test2;
+/* Difference Between Single & Double Cotation */
+	// $test = 10;
+	// $data = "Hi my name Bappa Please $test";
+	// $data = 'Hi my name Bappa Please $test';
+/* Difference Between Single & Double Cotation */
+// $data = 'Hello Bangladesh and My name is "Bappa Chy"';
+// $data = "Hello Bangladesh and My name is \"Bappa Chy\"";
+// $data = 'Hello Bangladesh and My name is \'Bappa Chy\'';
+ $test1 = 'Hello Bangladesh';
+ $test2 = 'Welcome To Bangladesh';
+// $data = $test1[4];
+// echo strlen($test2);
+
+/* How to create Function */
+// function test(){
+// 	echo "Hello my name is Bappa Chy";
+// }
+  
+// function test($content){
+// 	echo $content;
+// }
+
+// function test($content="Hello Bangladesh"){
+// 	echo $content;
+// }
+
+// function test($name,$location){
+// 	echo "My name is $name";
+// 	echo "<br>";
+// 	echo "I'm from $location";
+// }
+
+// function test($name,$location){
+// 	echo 'My name is '.$name;
+// 	echo "<br>";
+// 	echo 'I\'m from '.$location;
+// }
+
+echo strtoupper($test2);
+echo strlen($test2);
+echo strtolower($test2);
+echo str_replace('e', 'a', $test2);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+<h1></h1>
+<h2><?php ?></h2>
+</body>
+</html>
