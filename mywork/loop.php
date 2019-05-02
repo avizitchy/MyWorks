8a26750bc02081afdd361c8f51ea1facf7a23740
Avizit Chowdhury <49820912+avizitchy@users.noreply.github.com>
2019-05-02 23:23:28 +0600

    Add files via upload

---
+++ mywork/loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop</title>
+</head>
+<body>
+
+	<?php 
+		for($i = 1; $i < 100; $i++ ) {
+			echo $i . "</br>";
+		}
+	 ?>
+
+	
+</body>
+</html>
